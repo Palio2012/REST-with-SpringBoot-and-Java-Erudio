a9148ee6f7452cce3c8bd5404f4640c31d08025a
Daniel Asrayan Baldassi <daniasrayan.com@gmail.com>
2023-10-06 09:15:39 -0300

    Hello World!

---
+++ REST-with-SpringBoot-and-Java-Erudio/src/main/java/br/com/erudio/Startup/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.erudio.Startup;
+
+public class Greeting {
+
+    private final long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
